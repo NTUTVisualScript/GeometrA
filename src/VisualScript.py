ff0852852d9419cc072da986e2d2435d7babf048
黃品皓 <yamiyokou@gmail.com>
2017-07-05 16:51:44 +0800

    Fix Bug-redo undo

--- src/VisualScript.py
+++ src/VisualScript.py
@@ -656,12 +656,25 @@ class View(Frame, threading.Thread):
 
     def undo(self):
         if len(self.testcase_undo) != 0:
+            print("undo ",self.testcase_undo)
             testcase_line = self.testcase_undo.pop()
             line = testcase_line[0]
+
+            if str(type(self.valuelist[line])) == "<class 'TestCaseEntry.TestCaseValue'>":
+                add_redo = [line, "", self.actioncombolist[line].get(), self.valuelist[line].get(),
+                            self.valueImagelist[line], self.node_path_list[line]]
+            else:
+                add_redo = [line, "", self.actioncombolist[line].get(), self.valuelist[line].image,
+                            self.valueImagelist[line], self.node_path_list[line]]
+
+            self.testcase_redo.append(add_redo)
+
             print(line)
             if testcase_line[1] == "remove":
                 self.AddLineButtonClick(line, True)
                 self.actioncombolist[line].set(testcase_line[2])
+                self.actionlist[line] = testcase_line[2]
+                print("undo ", testcase_line[2])
                 if testcase_line[2] == "Click" or testcase_line[2] == "Assert Exist" or \
                      testcase_line[2] == "Assert Not Exist":
                     self.TestcaseImage(line, testcase_line[3])
@@ -674,21 +687,31 @@ class View(Frame, threading.Thread):
                 self.RemoveLineButtonClick(line, True)
             elif testcase_line[2] == "Click" or testcase_line[2] == "Assert Exist" or \
                             testcase_line[2] == "Assert Not Exist":
+                self.actioncombolist[line].set(testcase_line[2])
                 self.TestcaseImage(line, testcase_line[3])
             else:
                 self.TestcaseEntry(line)
+                self.actioncombolist[line].set(testcase_line[2])
                 self.valuelist[line].delete(0, END)
-                self.valuelist[line].insert(0, testcase_line[3])
+                if testcase_line[3]!= None:
+                   self.valuelist[line].insert(0, testcase_line[3])
 
             self.valueImagelist[line] = testcase_line[4]
             self.node_path_list[line] = testcase_line[5]
 
-            self.testcase_redo.append(testcase_line)
-
     def redo(self):
-        if self.testcase_redo != []:
+        if len(self.testcase_redo) != 0:
+            print("redo ",self.testcase_redo)
             testcase_line = self.testcase_redo.pop()
             line = testcase_line[0]
+
+            if str(type(self.valuelist[line])) == "<class 'TestCaseEntry.TestCaseValue'>":
+                add_undo = [line, "", self.actioncombolist[line].get(), self.valuelist[line].get(), self.valueImagelist[line], self.node_path_list[line]]
+            else:
+                add_undo = [line, "", self.actioncombolist[line].get(), self.valuelist[line].image, self.valueImagelist[line], self.node_path_list[line]]
+
+            self.testcase_undo.append(add_undo)
+
             self.actioncombolist[line].set(testcase_line[2])
             if testcase_line[1] == "remove":
                 self.RemoveLineButtonClick(line, True)
@@ -700,12 +723,13 @@ class View(Frame, threading.Thread):
             else:
                 self.TestcaseEntry(line)
                 self.valuelist[line].delete(0, END)
-                self.valuelist[line].insert(0, testcase_line[3])
+                if testcase_line[3] != None:
+                    self.valuelist[line].insert(0, testcase_line[3])
 
             self.valueImagelist[line] = testcase_line[4]
             self.node_path_list[line] = testcase_line[5]
 
-            self.testcase_undo.append(testcase_line)
+
 
     def add_changes(self, line, change, action, value, image, path):
         print(str(type(value)))
@@ -714,6 +738,7 @@ class View(Frame, threading.Thread):
         else:
             testcase_line = [line, change, action, value.image, image, path]
 
+        print(action)
         self.testcase_undo.append(testcase_line)
         del self.testcase_redo[:]
 
@@ -759,9 +784,9 @@ class View(Frame, threading.Thread):
     def AuxscrollFunction(self, event):
         self.canvas.configure(scrollregion=self.canvas.bbox("all"), width=650, height=525)
 
-    def TestcaseImage(self,line, image):
+    def TestcaseImage(self,line, image = None):
         #print("image : ",line)
-        values_image = Canvas(self.listFrame, height=100, width=100)
+        values_image = Canvas(self.listFrame, bg = "#FFFFFF",height=100, width=100)
         values_image.create_image(0, 0, anchor=NW, image=image)
         values_image.bind("<Button-1>", lambda event, i=line: self.valueFocusIn(event, i))
         values_image.image = image
@@ -819,6 +844,7 @@ class View(Frame, threading.Thread):
     def ActionSelect(self,event, n):
         self.focus = n
         #self.cmd.do(self.actioncombolist[n])
+        print(self.actionlist[n])
         self.add_changes(n, "", self.actionlist[n], self.valuelist[n], self.valueImagelist[n], self.node_path_list[n])
         self.actionlist[n] = self.actioncombolist[n].get()
         self.valueImagelist[n] = None
@@ -832,6 +858,7 @@ class View(Frame, threading.Thread):
 
     def Action_FocusIn(self):
         if self.actioncombolist[self.focus].get() == 'Drag':
+            if filePath is None: return
             self.Drag_image = Canvas(self.screenshot, height=800, width=450)
             self.Drag_image.configure(borderwidth=-3)
             self.Drag_image.place(x=0, y=0)
@@ -852,6 +879,8 @@ class View(Frame, threading.Thread):
                 if path !="":
                     self.valuelist[self.focus].delete(0, 'end')
                     self.valuelist[self.focus].insert('end', path)
+        elif self.actioncombolist[self.focus].get() == 'Click' or self.actioncombolist[self.focus].get() == 'Assert Exist' or self.actioncombolist[self.focus].get() == 'Assert Not Exist':
+            self.TestcaseImage(self.focus)
 
         else:
             if self.Drag_image != None:
