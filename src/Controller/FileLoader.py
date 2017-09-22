886d2528cc9368a986cd6132c7dee7a56e208fde
mario31274 <mario31274@yahoo.com.tw>
2017-09-22 11:49:32 +0800

    FileLoader is singleton now

--- src/Controller/FileLoader.py
+++ src/Controller/FileLoader.py
@@ -7,23 +7,18 @@ import Mouse
 from TestCaseUI import TestCaseUI
 from TestStep import Step
 
-class SaveFile:
-    __single = None
-    def __init__(self):
-        self._filePath = ""
-        self._folderPath = ""
-        self._fileName = ""
 
+class SaveFile:
     def saveButtonClick(self):
         self.saveFile()
 
     def saveFile(self):
-        self.getFilePath()
+        self.getSaveFilePath()
         self.getFolderName()
-        if self._filePath is '': return
+        if self._filePath == '': return
         self.jsonEncoder()
 
-    def getFilePath(self):
+    def getSaveFilePath(self):
         _f = filedialog.asksaveasfilename(title="Save as...", filetypes=[("TestCase JSON Files", "*.json")])
         if not _f is '':
             if '.json' not in _f:
@@ -31,9 +26,7 @@ class SaveFile:
             self._filePath = _f
             self._fileName = _f.split('/').pop().rstrip('.json')
 
-    def getFolderName(self):
-        _fp = self._filePath
-        self._folderPath = _fp.rstrip(self._fileName + '.json')
+
 
     def jsonEncoder(self):
         _dataDict = {}
@@ -44,7 +37,7 @@ class SaveFile:
             _data['action'] = act
             if str(_case.getSteps(i).getValue().__class__) == "<class 'PIL.PngImagePlugin.PngImageFile'>":
                 # Image path format: /*TestCaseName*_image/image_*StepNumber*.png
-                _imgpath = self._fileName + "_image/image_" + str(i+1) + ".png"
+                _imgpath = self._fileName + "_image/image_" + str(i + 1) + ".png"
                 if not os.path.isdir(self._folderPath + self._fileName + "_image"):
                     os.makedirs(self._folderPath + self._fileName + "_image")
                 _case.getSteps(i).getValue().save(self._folderPath + _imgpath)
@@ -60,33 +53,24 @@ class SaveFile:
         with open(self._filePath, 'w', encoding='utf-8') as fp:
             json.dump(_dataDict, fp, indent=2)
 
-class LoadFile:
-    def __init__(self):
-        self._filePath = ""
-        self._folderPath = ""
-        self._fileName = ""
-        self.case = None
 
+class LoadFile:
     def loadButtonClick(self):
         self.loadFile()
         TestCaseUI.getTestCaseUI().reloadTestCaseUI()
 
     def loadFile(self):
-        self.getFilePath()
+        self.getLoadFilePath()
         self.getFolderName()
         if self._filePath is None or self._filePath is "": return
         self.jsonDecoder()
 
-    def getFilePath(self):
+    def getLoadFilePath(self):
         self._filePath = ""
         _f = filedialog.askopenfile(title="Select File", filetypes=[("TestCase JSON Files", "*.json")])
         if _f is None: return
         self._filePath = _f.name
         self._fileName = _f.name.split('/').pop()
-
-    def getFolderName(self):
-        _fp = self._filePath
-        self._folderPath = _fp.rstrip(self._fileName)
 
     def jsonDecoder(self):
         with open(self._filePath, 'r') as f:
@@ -95,11 +79,11 @@ class LoadFile:
         self.case.clear()
 
         for i in range(len(dataDic)):
-            data = dataDic[str(i+1)]
+            data = dataDic[str(i + 1)]
 
             act = data['action']
             val = data['value']
-            if val == None:
+            if val is None:
                 val = Image.open(self._folderPath + data['image'])
 
             step = Step(act, val)
@@ -107,3 +91,25 @@ class LoadFile:
 
     def getTestCase(self):
         return TestCaseUI.getTestCaseUI().ctrl.case
+
+
+class FileLoader(SaveFile, LoadFile):
+    __single = None
+
+    def __init__(self):
+        if FileLoader.__single:
+            raise FileLoader.__single
+        FileLoader.__single = self
+        self._filePath = ""
+        self._folderPath = ""
+        self._fileName = ""
+        self.case = None
+
+    def getFileLoader(self):
+        if FileLoader.__single is None:
+            FileLoader.__single = FileLoader()
+        return FileLoader.__single
+
+    def getFolderName(self):
+        _fp = self._filePath
+        self._folderPath = _fp.rstrip(self._fileName + '.json')
