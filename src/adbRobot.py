51979558b1d31481f851463b213220b3c0de33a6
HuYuHsiang <nightlord851108@gmail.com>
2017-11-16 12:41:58 +0800

    Let the code be executable on Mac OS platform.

--- src/adbRobot.py
+++ src/adbRobot.py
@@ -7,15 +7,19 @@ import re
 PATH = lambda p: os.path.abspath(p)
 
 KEYCODE = ANDROID_KEYCODE
-subp = os.path.join(os.environ["Android_HOME"], "platform-tools", "adb.exe")
+subp = os.path.join(os.environ["Android_HOME"], "platform-tools", "adb")
 
 
 class ADBRobot(Robot):
     def open_app(self, appName):
-        subprocess.check_output([subp, "shell", "am", "start", "-n", appName ], shell=True)
+        # subprocess.check_output([subp, "shell", "am", "start", "-n", appName ], shell=True)
+        command = "adb shell am start -n " + appName
+        subprocess.check_output(command, shell=True)
 
     def close_app(self, appName):
-        subprocess.check_output([subp, "shell", "am", "force-stop", appName], shell=True)
+        # subprocess.check_output([subp, "shell", "am", "force-stop", appName], shell=True)
+        command = "adb shell am force-stop " + appName
+        subprocess.check_output(command, shell=True)
 
     def get_devices(self):
         dList = subprocess.getoutput('adb devices')
@@ -27,80 +31,118 @@ class ADBRobot(Robot):
             self.send_key(KEYCODE[key])
 
     def send_key(self, keycode):
-        return subprocess.call([subp, "shell", "input", "keyevent", str(keycode)], shell=True)
+        # return subprocess.call([subp, "shell", "input", "keyevent", str(keycode)], shell=True)
+        command = "adb shell input keyevent " + str(keycode)
+        return subprocess.call(command, shell=True)
 
     def drag_and_drop(self, start_x, start_y, end_x, end_y):
-        subprocess.call([subp, "shell", "input", "swipe", str(start_x), str(start_y), str(end_x), str(end_y) ], shell=True)
+        # subprocess.call([subp, "shell", "input", "swipe", str(start_x), str(start_y), str(end_x), str(end_y) ], shell=True)
+        command = " ".join(["adb shell input swipe", str(start_x), str(start_y), str(end_x), str(end_y)])
+        subprocess.call(command, shell=True)
 
     def screenshot(self):
-        path = PATH(os.getcwd() + "/screenshot_pic")
-        subprocess.call([subp, "wait-for-device"], shell=True)
-        subprocess.call([subp, "shell", "screencap", "-p", "/data/local/tmp/tmp.png"], shell=True)
-        if not os.path.isdir(PATH(os.getcwd() + "/screenshot_pic")):
+        path = "./screenshot_pic"
+        wait = "adb wait-for-device"
+        subprocess.call(wait, shell=True)
+        fileName = "tmp.png"
+        capture = "adb shell screencap -p ./data/local/tmp/" + fileName
+        subprocess.call(capture, shell=True)
+        if not os.path.isdir(path):
             os.makedirs(path)
-        subprocess.call([subp, "pull", "/data/local/tmp/tmp.png", str(PATH(path + "/tmp.png"))], shell=True)
+        pull = "adb pull ./data/local/tmp/" + fileName + " " + path
+        subprocess.call(pull ,shell=True)
         print("success")
-        return path + "/tmp.png"
+        return path + "/" + fileName
 
     def before_screenshot(self):
-        path = PATH(os.getcwd() + "/screenshot_pic")
-        subprocess.call([subp, "wait-for-device"], shell=True)
-        subprocess.call([subp, "shell", "screencap", "-p", "/data/local/tmp/before.png"], shell=True)
-        if not os.path.isdir(PATH(os.getcwd() + "/screenshot_pic")):
+        path = "./screenshot_pic"
+
+        # subprocess.call([subp, "wait-for-device"], shell=True)
+        wait = "adb wait-for-device"
+        subprocess.call(wait, shell=True)
+
+        # subprocess.call([subp, "shell", "screencap", "-p", "/data/local/tmp/before.png"], shell=True)
+        fileName = "before.png"
+        capture = "adb shell screencap -p ./data/local/tmp/" + fileName
+        subprocess.call(capture, shell=True)
+
+        if not os.path.isdir(path):
             os.makedirs(path)
-        subprocess.call([subp, "pull", "/data/local/tmp/before.png", str(PATH(path + "/before.png"))], shell=True)
+
+        # subprocess.call([subp, "pull", "/data/local/tmp/before.png", str(PATH(path + "/before.png"))], shell=True)
+        pull = "adb pull ./data/local/tmp/" + fileName + " " + path
+        subprocess.call(pull, shell=True)
         print("success")
-        return path + "/before.png"
+        return path + "/" + fileName
 
     def after_screenshot(self):
-        path = PATH(os.getcwd() + "/screenshot_pic")
-        subprocess.call([subp, "wait-for-device"], shell=True)
-        subprocess.call([subp, "shell", "screencap", "-p", "/data/local/tmp/after.png"], shell=True)
-        if not os.path.isdir(PATH(os.getcwd() + "/screenshot_pic")):
+        path = "./screenshot_pic"
+
+        # subprocess.call([subp, "wait-for-device"], shell=True)
+        wait = "adb wait-for-device"
+        subprocess.call(wait, shell=True)
+        # subprocess.call([subp, "shell", "screencap", "-p", "/data/local/tmp/after.png"], shell=True)
+        fileName = "after.png"
+        capture = "adb shell screencap -p ./data/local/tmp/" + fileName
+        subprocess.call(capture, shell=True)
+
+        if not os.path.isdir(path):
             os.makedirs(path)
-        subprocess.call([subp, "pull", "/data/local/tmp/after.png", str(PATH(path + "/after.png"))], shell=True)
+
+        # subprocess.call([subp, "pull", "/data/local/tmp/after.png", str(PATH(path + "/after.png"))], shell=True)
+        pull = "adb pull ./data/local/tmp/" + fileName + " " + path
+        subprocess.call(pull, shell=True)
         print("success")
         return path + "/after.png"
 
     def tap(self, x, y, duration=None):
         if duration:
-            subprocess.call(
-                [subp, "shell", "input", "swipe", str(x), str(y), str(x), str(y), str(3000)],
-                shell=True)
+            # subprocess.call(
+            #     [subp, "shell", "input", "swipe", str(x), str(y), str(x), str(y), str(3000)],
+            #     shell=True)
+            command = " ".join(["adb shell input swipe", str(x), str(y), str(x), str(y), str(3000)])
         else:
-            subprocess.call(
-                [subp, "shell", "input", "tap", str(x), str(y)],
-                shell=True)
+            # subprocess.call(
+            #     [subp, "shell", "input", "tap", str(x), str(y)],
+            #     shell=True)
+            command = " ".join(["adb shell input tap", str(x), str(y)])
 
-    def input_text(self, inputtext):
-        text = list(inputtext)
+        subprocess.call(command, shell=True)
+
+    def input_text(self, text):
         #print("text =" + str(text))
         try:
-            for i in range(len(text)):
-                print(text[i])
-                if text[i] == " ":
-                    subprocess.call([subp, "shell", "input", "text", "%s"], shell=True)
-                else:
-                    subprocess.call([subp, "shell", "input", "text", text[i]], shell=True)
+            text = text.replace(' ', '%s')
+            command = 'adb shell input text ' + text
+            subprocess.call(command, shell=True)
 
             return "Success"
         except:
-            print("Input Text Error", inputtext)
+            print("Input Text Error", text)
             return "Error"
 
     def get_uiautomator_dump(self):
-        path = PATH(os.getcwd() + "/dumpXML")
-        subprocess.call([subp, "wait-for-device"], shell=True)
-        subprocess.call([subp, "shell", "uiautomator", "dump", "/data/local/tmp/uidump.xml"], shell=True)
-        if not os.path.isdir(PATH(os.getcwd() + "/dumpXML")):
+        # path = PATH(os.getcwd() + "/dumpXML")
+        path = "./dumpXML"
+
+        # subprocess.call([subp, "wait-for-device"], shell=True)
+        wait = "adb wait-for-device"
+        subprocess.call(wait, shell=True)
+        # subprocess.call([subp, "shell", "uiautomator", "dump", "/data/local/tmp/uidump.xml"], shell=True)
+        fileName = "uidump.xml"
+        dump = "adb shell uiautomator dump ./data/local/tmp/" + fileName
+        subprocess.call(dump, shell=True)
+        if not os.path.isdir(path):
             os.makedirs(path)
-        subprocess.call([subp, "pull", "/data/local/tmp/uidump.xml", path + "/uidump.xml"], shell=True)
+        # subprocess.call([subp, "pull", "/data/local/tmp/uidump.xml", path + "/uidump.xml"], shell=True)
+        pull = "adb pull ./data/local/tmp/" + fileName + " " + path
+        subprocess.call(pull, shell=True)
         print(path + "/uidump.xml")
         return path + "/uidump.xml"
 
     def get_display(self):
         real_size_pattern = r"real (\d+) x (\d+),"
-        result = subprocess.check_output('adb shell dumpsys display | grep mBaseDisplayInfo').__str__()
+        result = subprocess.check_output('adb shell dumpsys display | grep mBaseDisplayInfo', shell=True).__str__()
         match = re.search(real_size_pattern, result)
         return (match.group(1), match.group(2))
 
--- test/test_ExecutorAction.py
+++ test/test_ExecutorAction.py
@@ -13,21 +13,21 @@ from TestStep import Step
 from TestCase import TestCase
 
 class ExecutorActionTestSuite(unittest.TestCase):
-    # def testExecuteClick(self):
-    #     case = TestCase()
-    #     exe = Executor(case)
-    #     case.insert(act='Android Keycode', val='KEYCODE_HOME')
-    #     case.insert(act='Click', val=Image.open('./TestCase/Test/image/exist.png'))
-    #     exe.execute(0)
-    #     self.assertEqual('Success', exe.execute(1))
-    #     exe.execute(0)
-    # def testExecuteClickNotFound(self):
-    #     case = TestCase()
-    #     exe = Executor(case)
-    #     case.insert(act='Android Keycode', val='KEYCODE_HOME')
-    #     case.insert(act='Click', val=Image.open('./TestCase/Test/image/notexist.png'))
-    #     exe.execute(0)
-    #     self.assertEqual('Failed', exe.execute(1))
+    def testExecuteClick(self):
+        case = TestCase()
+        exe = Executor(case)
+        case.insert(act='Android Keycode', val='KEYCODE_HOME')
+        case.insert(act='Click', val=Image.open('./TestCase/Test/image/exist.png'))
+        exe.execute(0)
+        self.assertEqual('Success', exe.execute(1))
+        exe.execute(0)
+    def testExecuteClickNotFound(self):
+        case = TestCase()
+        exe = Executor(case)
+        case.insert(act='Android Keycode', val='KEYCODE_HOME')
+        case.insert(act='Click', val=Image.open('./TestCase/Test/image/notexist.png'))
+        exe.execute(0)
+        self.assertEqual('Failed', exe.execute(1))
 
     def testExecuteSetText(self):
         case = TestCase()
@@ -42,45 +42,45 @@ class ExecutorActionTestSuite(unittest.TestCase):
         self.assertEqual('Success', exe.imageFinder(step=target))
         exe.execute(2)
 
-    # def testExecuteTestCase(self):
-    #     case = TestCase()
-    #     exe = Executor(case)
-    #     case.insert(act='TestCase', val='./TestCase/Test/testcase.json')
-    #     self.assertEqual('Success', exe.execute(0))
-    #
-    # def testExecuteAssertExist(self):
-    #     case = TestCase()
-    #     exe = Executor(case)
-    #     case.insert(act='Android Keycode', val='KEYCODE_HOME')
-    #     case.insert(act='Assert Exist', val=Image.open('./TestCase/Test/image/exist.png'))
-    #     exe.execute(0)
-    #     self.assertEqual('Success', exe.execute(1))
-    #     exe.execute(0)
-    # def testExecuteAssertExistFailed(self):
-    #     case = TestCase()
-    #     exe = Executor(case)
-    #     case.insert(act='Assert Exist', val=Image.open('./TestCase/Test/image/notexist.png'))
-    #     self.assertEqual('Failed', exe.execute(0))
-    #
-    # def testExecuteAssertNotExist(self):
-    #     case = TestCase()
-    #     exe = Executor(case)
-    #     case.insert(act='Assert Not Exist', val=Image.open('./TestCase/Test/image/notexist.png'))
-    #     self.assertEqual('Success', exe.execute(0))
-    # def testExecuteAssertNotExist(self):
-    #     case = TestCase()
-    #     exe = Executor(case)
-    #     case.insert(act='Assert Not Exist', val=Image.open('./TestCase/Test/image/exist.png'))
-    #     self.assertEqual('Failed', exe.execute(0))
-    #
-    # def testExecuteNestLoop(self):
-    #     case = TestCase()
-    #     exe = Executor(case)
-    #     case.insert(act='Loop Begin', val='2')
-    #     case.insert(act='Sleep(s)', val='0')
-    #     case.insert(act='Loop Begin', val='2')
-    #     case.insert(act='Swipe', val='start x=500, y=1000, end x=300, y=1000')
-    #     case.insert(act='Loop End', val=None)
-    #     case.insert(act='Android Keycode', val = 'KEYCODE_HOME')
-    #     case.insert(act='Loop End', val=None)
-    #     self.assertEqual('Success', exe.execute(0))
+    def testExecuteTestCase(self):
+        case = TestCase()
+        exe = Executor(case)
+        case.insert(act='TestCase', val='./TestCase/Test/testcase.json')
+        self.assertEqual('Success', exe.execute(0))
+
+    def testExecuteAssertExist(self):
+        case = TestCase()
+        exe = Executor(case)
+        case.insert(act='Android Keycode', val='KEYCODE_HOME')
+        case.insert(act='Assert Exist', val=Image.open('./TestCase/Test/image/exist.png'))
+        exe.execute(0)
+        self.assertEqual('Success', exe.execute(1))
+        exe.execute(0)
+    def testExecuteAssertExistFailed(self):
+        case = TestCase()
+        exe = Executor(case)
+        case.insert(act='Assert Exist', val=Image.open('./TestCase/Test/image/notexist.png'))
+        self.assertEqual('Failed', exe.execute(0))
+
+    def testExecuteAssertNotExist(self):
+        case = TestCase()
+        exe = Executor(case)
+        case.insert(act='Assert Not Exist', val=Image.open('./TestCase/Test/image/notexist.png'))
+        self.assertEqual('Success', exe.execute(0))
+    def testExecuteAssertNotExist(self):
+        case = TestCase()
+        exe = Executor(case)
+        case.insert(act='Assert Not Exist', val=Image.open('./TestCase/Test/image/exist.png'))
+        self.assertEqual('Failed', exe.execute(0))
+
+    def testExecuteNestLoop(self):
+        case = TestCase()
+        exe = Executor(case)
+        case.insert(act='Loop Begin', val='2')
+        case.insert(act='Sleep(s)', val='0')
+        case.insert(act='Loop Begin', val='2')
+        case.insert(act='Swipe', val='start x=500, y=1000, end x=300, y=1000')
+        case.insert(act='Loop End', val=None)
+        case.insert(act='Android Keycode', val = 'KEYCODE_HOME')
+        case.insert(act='Loop End', val=None)
+        self.assertEqual('Success', exe.execute(0))
