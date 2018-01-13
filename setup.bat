aafee3704a7f49fad1fdae6c480b5967e35ddfb5
HuYuHsiang <hsiang851108@gmail.com>
2018-01-13 11:53:15 +0800

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,8 +1,8 @@
-pip install Pillow
-pip install nose
-pip install opencv-python
+pip3 install Pillow
+pip3 install nose
+pip3 install opencv-python
 setx /m Android_HOME "%USERPROFILE%\AppData\Local\Android\sdk"
 setx /m PATH "%PATH%;%Android_HOME%\platform-tools"
-pip install flask
-pip install -U flask-cors
+pip3 install flask
+pip3 install -U flask-cors
 PAUSE
