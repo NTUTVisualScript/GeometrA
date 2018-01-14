e6cd0f2af320db38b759ed0f83c5d249648d626e
Nightlord851108 <nightlord851108@gmail.com>
2018-01-14 21:45:30 +0800

    Fixed workspace and it's unittest problem.

--- VisualScript/src/File/WorkSpace.py
+++ VisualScript/src/File/WorkSpace.py
@@ -3,16 +3,15 @@ import os
 from VisualScript.src.File.Project import Project
 
 class WorkSpace:
-    def __init__(self, path, project):
+    def __init__(self, path=None, project=None):
         self.projects = {}
-        self.add(path, project)
+        if path and project:
+            self.add(path, project)
 
     def add(self, path, project):
         name = project[0]
 
         if not os.path.isdir(path + "/" + name):
-            print(path + '/' + name)
-            print(os.path.isdir(path + '/' + name))
             raise Exception('Project: "' + name + '" is not in the path')
 
         projectPath = path + '/' + name
@@ -22,7 +21,7 @@ class WorkSpace:
         if not p in self.projects:
             raise Exception('Project: "' + name +'" not exist')
         d = self.projects[p].getJSON()
-        result = {p:d}
+        result = [p, {p:d}]
         return result
 
     def getTreeJSON(self):
--- tests/File/test_WorkSpace.py
+++ tests/File/test_WorkSpace.py
@@ -6,24 +6,24 @@ from VisualScript.src.File.WorkSpace import WorkSpace
 class WorkSpaceTestSuite(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        shutil.rmtree('./Project1', True)
-        shutil.rmtree('./Project2', True)
+        shutil.rmtree('./File/Project1', True)
+        shutil.rmtree('./File/Project2', True)
 
     def setUp(self):
-        shutil.copytree('./Project', './Project1')
-        shutil.copytree('./Project', './Project2')
-        self.path = os.getcwd()
+        shutil.copytree('./File/Project', './File/Project1')
+        shutil.copytree('./File/Project', './File/Project2')
+        self.path = os.getcwd() + '/File'
 
     def tearDown(self):
-        shutil.rmtree('./Project1', True)
-        shutil.rmtree('./Project2', True)
+        shutil.rmtree('./File/Project1', True)
+        shutil.rmtree('./File/Project2', True)
 
     def testConstructor(self):
         p = ['Project1', {'Project1':{'Suite1': ['case1', 'case2'],
                           'Suite2': ['case2']}}]
         path = self.path
         ws = WorkSpace(path, p)
-        self.assertEqual("<class 'File.WorkSpace.WorkSpace'>", str(ws.__class__))
+        self.assertEqual("<class 'VisualScript.src.File.WorkSpace.WorkSpace'>", str(ws.__class__))
         self.assertEqual(1, len(ws.projects))
         self.assertEqual(True, 'Project1' in ws.projects)
     def testConstructorExcept(self):
@@ -37,7 +37,7 @@ class WorkSpaceTestSuite(unittest.TestCase):
         p = ['Project1', {'Project1':{'Suite1': ['case1', 'case2'],
                           'Suite2': ['case2']}}]
         ws = WorkSpace(self.path, p)
-        self.assertEqual(p[1], ws.getJSON('Project1'))
+        self.assertEqual(p, ws.getJSON('Project1'))
 
     def testGetTreeJSON(self):
         p1 = ['Project1', {'Project1':{'Suite1': ['case1', 'case2'],
