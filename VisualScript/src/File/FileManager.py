b735d780798ae90be95c6e1fa0e0da68d2eab4c2
Nightlord851108 <nightlord851108@gmail.com>
2018-01-29 16:57:27 +0800

    Change the way new function add new project to workspace.

--- VisualScript/src/File/FileManager.py
+++ VisualScript/src/File/FileManager.py
@@ -9,32 +9,13 @@ def new(info):
     projectPath = path + '/' + info['project']
     if os.path.isdir(projectPath):
         raise Exception('The directory is exist!')
-    suitePath = projectPath + '/' + info['suite']
-    casePath = suitePath + '/' + info['case']
+    project = info['project']
+    suite = info['suite']
+    case = info['case']
 
-    # Make the directories and the files
-    os.mkdir(projectPath)
-    os.mkdir(suitePath)
-    os.mkdir(casePath)
-    with open(casePath + '/testcase.json', 'w') as f:
-        f.write(json.dumps({}))
-
-    writeJSON(info, projectPath)
-
-def writeJSON(info, path):
-    # Get the path of record file
-    jsonPath = path + '/' + info['project'] + '.json'
-
-    # Encode a json object
-    log = json.dumps([info['project'], {
-        info['project']: {
-            info['suite']: [info['case'], ],
-        },
-    }])
-
-    # Out put the file.
-    with open(jsonPath, 'w') as f:
-        f.write(log)
+    WORKSPACE.add(path, project)
+    WORKSPACE.projects[project].add(suite)
+    WORKSPACE.projects[project].add(suite, case)
 
 def load(path, workspace=None):
     # ws as a component for unittest
@@ -45,6 +26,6 @@ def load(path, workspace=None):
         data = json.load(f)
     try:
         projectPath = path[:path.rindex('/', 0, path.rindex('/'))]
-        workspace.add(projectPath, data)
+        workspace.load(projectPath, data)
     except Exception as e:
         raise e
