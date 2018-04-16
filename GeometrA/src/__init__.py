d171553e61f0606f6162e092254e7e7d4b49d55e
Nightlord851108 <nightlord851108@gmail.com>
2018-04-16 11:18:36 +0800

    Successfully get screen shot from android device with "Dump Screen" button.

--- GeometrA/src/__init__.py
+++ GeometrA/src/__init__.py
@@ -1,5 +1,7 @@
 from GeometrA.src.File.WorkSpace import WorkSpace
 from GeometrA.src.TestScript import TestScript
+from GeometrA.src.Screen import Screen
 
 WORKSPACE = WorkSpace()
 TESTSCRIPT = TestScript()
+# SCREEN = Screen()
