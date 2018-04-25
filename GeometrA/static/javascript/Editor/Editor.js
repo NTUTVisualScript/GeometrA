2b5fdd0fbe15c7a450be035ac3dd09621ea5a6a5
TsaiWenXue <wto2005716@gmail.com>
2018-04-25 14:22:18 +0800

    remove unnecessary code of tab

--- GeometrA/static/javascript/Editor/Editor.js
+++ GeometrA/static/javascript/Editor/Editor.js
@@ -4,15 +4,15 @@ $(document).ready(function(){
 
     $("#tabs").on("tabsactivate", function(){
         var active = $('#tabs ul .ui-tabs-active').attr("id");
-        // var target = active.attr("id");
-        // console.log(active);
         var data = JSON.parse(active);
         Post('/GeometrA/WorkSpace/open', data, function(xml){
             openTestCase(xml);
         });
     });
+
     $("#tabs").on( "click", "span.ui-icon-close", function() {
         var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
+
         $( "#" + panelId ).remove();
         if($("div#tabs ul li").length == 0){
             Blockly.mainWorkspace.clear();
