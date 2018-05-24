9877731ca20738c9eacef4ca92b20bb0fb444f65
Nightlord851108 <nightlord851108@gmail.com>
2018-05-24 21:37:09 +0800

    Connect NodeTree objects to ScreenShot.

--- GeometrA/static/javascript/NodeTree.js
+++ GeometrA/static/javascript/NodeTree.js
@@ -25,10 +25,42 @@ function NodeTree(){
     });
 
 
-    $("#Nodes").on("dblclick.jstree", ".jstree-anchor", function() {
-        var tree = $.jstree.reference(this);
-        var node = tree.get_node(this);
-        console.log("jkk");
-    });
+    // $("#Nodes").on("dblclick.jstree", ".jstree-anchor", function() {
+    //     var tree = $.jstree.reference(this);
+    //     var node = tree.get_node(this);
+    //
+    // });
+    var widthMulti;
+    var heightMulti;
+    function getPos() {
+        var screenElement = document.getElementById("ScreenCapture");
+        var bound = (screenElement.getBoundingClientRect());
+        // We should get real number instead of 1080x1920 as default.
+        widthMulti = 1080/bound.width;
+        heightMulti = 1920/bound.height;
+    }
 
+    $("#Nodes").on('changed.jstree', function (e, data) {
+        getPos();
+        var tree = $.jstree.reference(data.selected);
+        var bounds = tree.get_node(data.selected).data.bounds;
+        var coor = bounds.split('][');
+        var coor_start = coor[0].substring(1, coor[0].length).split(',');
+        var coor_end = coor[1].substring(0, coor[1].length-1).split(',');
+        var x1 = parseInt(coor_start[0]) / widthMulti;
+        var y1 = parseInt(coor_start[1]) / heightMulti;
+        var x2 = parseInt(coor_end[0]) / widthMulti;
+        var y2 = parseInt(coor_end[1]) / heightMulti;
+
+        var canvas = document.getElementById('Screen');
+        element = document.createElement('div');
+        element.className = 'rectangle'
+        element.style.left = x1 + 'px';
+        element.style.top = y1 + 'px';
+        element.style.width = x2 - x1;
+        element.style.height = y2 - y1;
+        canvas.removeChild(canvas.lastChild);
+        canvas.appendChild(element);
+        canvas.style.cursor = "crosshair";
+    });
 }
