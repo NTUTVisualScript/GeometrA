4ea2765c08ddcff34cc301f7896461a5dd987dc9
Patrick <nightlord851108@gmail.com>
2018-12-06 16:59:29 +0800

    Add File.TestSuiteTestSuite unittests

--- tests/File/__init__.py
+++ tests/File/__init__.py
@@ -1,3 +1,4 @@
 from tests.File.test_FileManager import FileManagerTestSuite
 from tests.File.test_Project import ProjectTestSuite
 from tests.File.test_TestCase import TestCaseTestSuite
+from tests.File.test_TestSuite import TestSuiteTestSuite
