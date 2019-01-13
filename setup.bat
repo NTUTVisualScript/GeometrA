485d9b02791c8ca4d7b05972ced2a7a27e3b537f
Patrick <nightlord851108@gmail.com>
2019-01-13 18:19:52 +0800

    Delete unused files

--- setup.bat
+++
@@ -1,9 +0,0 @@
-pip3 install Pillow
-pip3 install nose
-pip3 install opencv-python
-setx /m Android_HOME "%USERPROFILE%\AppData\Local\Android\sdk"
-setx /m PATH "%PATH%;%Android_HOME%\platform-tools"
-pip3 install flask
-pip3 install -U flask-cors
-npm install electron-prebuilt -g
-PAUSE
