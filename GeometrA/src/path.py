08c0e11d8e683fc31b03670b8382c83cd5762435
patrick.hu <patrick.hu@linecorp.com>
2019-01-14 16:56:30 +0800

    Try to fix firewall issue

--- GeometrA/src/path.py
+++ GeometrA/src/path.py
@@ -12,6 +12,6 @@ else:
     RESOURCE_PATH = file_path[0:file_path.rindex('/', 0, file_path.rindex('/', 0, file_path.rindex('/')))] + '/resources'
 
 if (not os.path.isdir(GEOMETRA_ROOT)):
-    if (os.path.isfile()):
+    if (os.path.isfile(GEOMETRA_ROOT)):
         os.remove(GEOMETRA_ROOT)
     os.mkdir(GEOMETRA_ROOT)
